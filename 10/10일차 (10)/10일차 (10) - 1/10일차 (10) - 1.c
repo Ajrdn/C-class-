db178c555dd6d7042885899e6013cf6c3d826d9c
Ajrdn <ajrdn1276@gmail.com>
2022-02-19 14:37:40 +0900

    구구단을 출력하게 하는 코드

---
+++ 10/10일차 (10)/10일차 (10) - 1/10일차 (10) - 1.c
@@ -0,0 +1,12 @@
+//구구단을 출력하게 하는 코드
+#include <stdio.h>
+int main()
+{
+   for(int i=2; i<=9; i++)
+   {
+      for(int j=1; j<=9; j++)
+      {
+         printf("%d * %d = %d\n", i, j, i*j);
+      }
+   }
+}
