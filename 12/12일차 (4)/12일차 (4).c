92b03771df5f6e99a3d16ec44b6c3a5208094961
Ajrdn <ajrdn1276@gmail.com>
2022-02-22 11:31:55 +0900

    소문자를 대문자로 바꿔서 출력하게 하는 코드

---
+++ 12/12일차 (4)/12일차 (4).c
@@ -0,0 +1,13 @@
+//소문자를 대문자로 바꿔서 출력하게 하는 코드
+#include <stdio.h>
+int main() 
+{
+  char a[101];
+  int i;
+  scanf ("%s", a);
+  for (i = 0; a[i] != '\0'; i++)
+    {
+      printf ("%c", a[i] - 32);
+    }
+    return 0;
+}
