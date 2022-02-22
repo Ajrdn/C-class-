cfa2c501ea0c8ed228639aed8e20994cc8d71c23
Ajrdn <ajrdn1276@gmail.com>
2022-02-22 11:25:14 +0900

    학생 5명의 성적를 입력받아 합계와 평균을 산출하게 하는 코드

---
+++ 12/12일차 (1)/12일차 (1).c
@@ -0,0 +1,17 @@
+//학생 5명의 성적를 입력받아 합계와 평균을 산출하게 하는 코드
+#include <stdio.h>
+int main() 
+{
+    int score[5];
+    int i, sum = 0;
+    for (i=0; i<5; i++)
+    { 
+        printf ("%d번 학생의 프로그래밍 성적을 입력하시오. : ", i+1);
+        scanf ("%d", &score[i]);
+    }
+  for (i=0; i<5; i++)
+    sum += score[i];
+  printf ("합계 : %d\n", sum);
+  printf ("평균 : %.2lf\n", (double)sum/5);
+    return 0;
+}
